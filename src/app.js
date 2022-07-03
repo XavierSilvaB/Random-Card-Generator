b6840fa9f6acf61be0f9bb6609c8e2b120969fca
XavierSilvaB <xavieraugustosilvabaute@gmail.com>
2022-07-03 01:36:37 +0000

    Ejer cicio Rndom Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+import "../src/style.css";
+let generateRandomNumber = () => {
+  const card = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let position = Math.floor(Math.random() * card.length);
+  return card[position];
+};
+let generateRandomSuit = () => {
+  const pinta = ["♦", "♥", "♠", "♣"];
+  let indexPinta = Math.floor(Math.random() * pinta.length);
+  return pinta[indexPinta];
+};
+let randomSuit = generateRandomSuit();
+let randomNumber = generateRandomNumber();
+window.onload = () => {
+  let topSuit = document.querySelector(".top-suit");
+  let number = document.querySelector(".number");
+  let bottomSuit = document.querySelector(".bottom-suit");
+  topSuit.innerHTML = randomSuit;
+  number.innerHTML = randomNumber;
+  bottomSuit.innerHTML = randomSuit;
+  if (randomSuit == ":diamantes:" || randomSuit == ":corazones:") {
+    topSuit.style.color = "red";
+    bottomSuit.style.color = "red";
+  }
 };
